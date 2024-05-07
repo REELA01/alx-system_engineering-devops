f662bc70fd29afedb7cf36da6672b022fd4f5cec
reela <sudanrela123@gmail.com>
2024-05-07 16:59:28 -0700

    tst

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""contain recurse function"""
+"""Function to query a list of all hot posts on a given Reddit subreddit."""
 import requests
 
 
@@ -7,8 +7,7 @@ def recurse(subreddit, hot_list=[], after="", count=0):
     """Returns a list of titles of all hot posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "0x16-api_advanced:project:\
-v1.0.0 (by /u/firdaus_cartoon_jr)"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     params = {
         "after": after,
@@ -19,6 +18,7 @@ v1.0.0 (by /u/firdaus_cartoon_jr)"
                             allow_redirects=False)
     if response.status_code == 404:
         return None
+
     results = response.json().get("data")
     after = results.get("after")
     count += results.get("dist")
