3d3545c3d68902e354426fb3359ec03fde7d0521
reela <sudanrela123@gmail.com>
2024-05-07 17:06:57 -0700

    tst

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,28 +1,37 @@
 #!/usr/bin/python3
-"""Function to query a list of all hot posts on a given Reddit subreddit."""
+"""
+Recursive function that queries the Reddit API and returns
+a list containing the titles of all hot articles for a given subreddit.
+If no results are found for the given subreddit,
+the function should return None.
+"""
+
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=None):
-    """Base case: if subreddit is invalid, return None"""
-    if subreddit is None:
-        return None
-    
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+def recurse(subreddit, hot_list=[], after=""):
+    """
+    Queries the Reddit API and returns
+    a list containing the titles of all hot articles for a given subreddit.
 
-    headers = {'User-Agent': 'MyBot/0.1'}
-    params = {'limit': 100, 'after': after}
+    - If not a valid subreddit, return None.
+    """
+    req = requests.get(
+        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+        params={"after": after},
+    )
 
-    response = requests.get(url, headers=headers, params=params)
-    if response.status_code == 200:
-        data = response.json()
-        articles = data['data']['children']
-        for article in articles:
-            hot_list.append(article['data']['title']
-        after = data['data']['after']
-        if after is not None:
-            recurse(subreddit, hot_list, after)
-        else:
+    if req.status_code == 200:
+        for get_data in req.json().get("data").get("children"):
+            dat = get_data.get("data")
+            title = dat.get("title")
+            hot_list.append(title)
+        after = req.json().get("data").get("after")
+
+        if after is None:
             return hot_list
+        else:
+            return recurse(subreddit, hot_list, after)
     else:
         return None
