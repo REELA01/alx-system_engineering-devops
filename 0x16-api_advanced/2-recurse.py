75e108d4a1b5e43e3445ac20c86fc66e3fd71351
reela <sudanrela123@gmail.com>
2024-05-07 17:04:44 -0700

    tst

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -8,35 +8,21 @@ def recurse(subreddit, hot_list=[], after=None):
     if subreddit is None:
         return None
     
-    # Reddit API URL for getting hot articles in the subreddit
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    
-    # Set the User-Agent header to avoid Too Many Requests error
+
     headers = {'User-Agent': 'MyBot/0.1'}
-    
-    # Set parameters for pagination
     params = {'limit': 100, 'after': after}
-    
-    # Send GET request to the Reddit API
+
     response = requests.get(url, headers=headers, params=params)
-    
-    # Check if the response is successful (status code 200)
     if response.status_code == 200:
-        # Parse JSON response
         data = response.json()
-        # Extract titles of hot articles from response
         articles = data['data']['children']
         for article in articles:
-            hot_list.append(article['data']['title'])
-        
-        # Check if there are more pages of results
+            hot_list.append(article['data']['title']
         after = data['data']['after']
         if after is not None:
-            # Recursively call the function to get next page of results
             recurse(subreddit, hot_list, after)
         else:
-            # If no more pages, return the list of titles
             return hot_list
     else:
-        # If subreddit is invalid or not found, return None
         return None
